7580bdc46d44f25a5ae13a61f32632338ec553e4
Fabian De Greef <fabian.degreef@student.pxl.be>
2018-09-25 16:43:09 +0200

    Initial Commit

---
+++ NutrientsTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NutrientsTracker
+//
+//  Created by Fabian De Greef on 25/09/2018.
+//  Copyright © 2018 Fabian De Greef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
